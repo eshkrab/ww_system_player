8005e03f3b22e1fa00fb23007451a18bed4cad16
rina shkrabova <eshkrab@gmail.com>
2023-06-20 15:16:33 -0700

    await process

--- app/app.py
+++ app/app.py
@@ -204,7 +204,7 @@ class PlayerApp:
             while True:
                 message = await sub_sock.recv_string()
                 logging.debug(f"Received message: {message}")
-                process_message(message)
+                await process_message(message)
                 await asyncio.sleep(0.1)
 
         finally:
